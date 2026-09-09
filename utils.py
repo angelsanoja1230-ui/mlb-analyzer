c406a36e05d19c15c358f9608b512cb1298eca5f
angelsanoja1230-ui <angelsanoja1230@gmail.com>
2026-09-08 23:35:31 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 # utils.py
 
 def limpiar_partidos_mlb(raw_data):
-    """Procesa el JSON bruto y devuelve una lista de diccionarios limpios."""
+    """Procesa el JSON de la MLB y devuelve una lista de diccionarios limpios."""
     clean_games = []
     
     games_list = raw_data if isinstance(raw_data, list) else raw_data.get('games', [])
@@ -20,7 +20,12 @@ def limpiar_partidos_mlb(raw_data):
             "has_2b": bool(g.get("has_2b", g.get("second", False))),
             "has_3b": bool(g.get("has_3b", g.get("third", False))),
             "logo_away": g.get("logo_away", ""),
-            "logo_home": g.get("logo_home", "")
+            "logo_home": g.get("logo_home", ""),
+            # Campos indispensables para que el frontend filtre los partidos en vivo
+            "abstract_state": g.get("abstract_state", ""),
+            "detailed_state": g.get("detailed_state", ""),
+            "time": g.get("time", ""),
+            "stadium": g.get("stadium", "")
         }
         clean_games.append(game_item)
         
